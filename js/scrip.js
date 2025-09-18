51ce1e0681da52e5aff0effcf280a5f9401890d2
Aleiaco02 <alessandroiacovelli83@gmail.com>
2025-09-18 18:00:16 +0200

    esercizio copleto

--- js/scrip.js
+++ js/scrip.js
@@ -1,33 +1,46 @@
 
-// dichiarazione variabili
 let containerMain = document.querySelector(".container");
+const overlay = document.getElementById("overlay");
+const overlayImg = document.getElementById("overlayImg");
+const closeBtn = document.getElementById("closeBtn");
 
-// assegnazione endpoint a variabile
-const endPoint = "https://lanciweb.github.io/demo/api/pictures/";
-
-// chiamata ajax api img
-axios.get(endPoint)
+axios.get("https://lanciweb.github.io/demo/api/pictures/")
     .then(elements => {
-        let outputString = "";
-
         elements.data.forEach(singleElement => {
-            console.log(singleElement);
+            const cardElement = document.createElement('div');
+            cardElement.classList.add('card', 'sizing-card', 'no-border-radius');
 
-            outputString += `
-                <div class="card sizing-card no-border-radius">
-                    <div>
-                        <img class="debug" src="${singleElement.url}" alt="${singleElement.url}">
-                    </div>
-                    <div class="card-body">
-                        <span>${singleElement.date}</span>
-                        <p class="card-text">Some quick example text to build on the card</p>
-                    </div>
+            cardElement.innerHTML = `
+            <a href="#">
+                <div>
+                    <img class="debug" src="${singleElement.url}" alt="${singleElement.url}">
                 </div>
+                <div class="card-body">
+                    <span>${singleElement.date}</span>
+                    <p class="card-text">Some quick example text</p>
+                </div>
+            </a>
             `;
-        });
-        containerMain.innerHTML = outputString;
 
-    })
+            // al click mostra overlay
+            cardElement.addEventListener('click', (e) => {
+                e.preventDefault();
+                overlayImg.src = singleElement.url;
+                overlay.style.display = "flex"; // mostra overlay
+            });
+
+            containerMain.append(cardElement);
+        });
+});
+
+// chiudi overlay
+closeBtn.addEventListener("click", () => {
+    overlay.style.display = "none";
+});
+overlay.addEventListener("click", e => {
+    if (e.target === overlay) overlay.style.display = "none";
+});
+
 
 
 
